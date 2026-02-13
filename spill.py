c5048afa73caa135c3002d8e8caf6c05ebf2b2a8
GitOnTheOtherHub <seb.jr@icloud.com>
2026-02-13 10:44:45 +0100

    endringer

--- spill.py
+++ spill.py
@@ -1,24 +1,6 @@
-import pygame
-import math
+import pygame as pg
+import math as m
 
-pygame.display.set_caption("infiltrering")
-win_width, win_height = (500, 500)
-display = pygame.display.set_mode((win_width, win_height))
-
-enviornment = [
-    [1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1],
-    [1, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1]
-]
-fov = 80
-xpos, ypos = (1, 1)
-rot_r = math.pi/4
-
-run = True
-while run:
-    
-    for e in pygame.event.get():
-        if e.type == quit:
-            run = False
+pg.init()
+screen = pg.display.set_mode((800,600))
+running = True
