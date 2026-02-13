82b4da1d7134349aa995ae1a6fa9b4532f202e64
GitOnTheOtherHub <seb.jr@icloud.com>
2026-02-13 11:16:53 +0100

    små endringer

--- spill.py
+++ spill.py
@@ -1,6 +1,19 @@
 import pygame as pg
-import math as m
+import numpy as np
 
 pg.init()
-screen = pg.display.set_mode((800,600))
+screen = pg.display.set_mode((500, 500))
 running = True
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    frame = np.random.uniform(0,1, (80, 60, 3))
+    surf = pg.surfarray.make_surface(frame*255)
+    surf = pg.transform.scale(surf, (500, 500))
+
+    screen.blit(surf, (0, 0))
+    pg.display.update()
+
+pg.quit()
