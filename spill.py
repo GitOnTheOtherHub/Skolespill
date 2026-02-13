7dc6d61734110e42774008556025f9b8edf9262b
GitOnTheOtherHub <seb.jr@icloud.com>
2026-02-13 09:02:25 +0100

    first commit

---
+++ spill.py
@@ -0,0 +1,14 @@
+import pygame
+import math
+
+pygame.display.set_caption("infiltrering")
+win_width, win_height = (500, 500)
+display = pygame.display.set_mode((win_width, win_height))
+
+enviornment = [
+    [],
+    [],
+    [],
+    [],
+    []
+]
