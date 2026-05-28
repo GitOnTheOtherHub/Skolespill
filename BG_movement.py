9667aded68bf59939d2db4c67dbe8774e68fab6f
GitOnTheOtherHub <seb.jr@icloud.com>
2026-05-28 14:05:43 +0200

    uhygtf

--- BG_movement.py
+++ BG_movement.py
@@ -6,34 +6,54 @@ pygame.init()
 timer = pygame.time.Clock
 fps = 60
 
-SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 500
+WIDTH = 700
+HEIGHT = 500
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption ("Spill")
 
-
-
+square_size = 50
+square_x = WIDTH // 1
+square_y = HEIGHT // 2
+speed = 0.07
 
 #lader inn bildet
 bg = pygame.image.load("background.jpg").convert()
 bg_width = bg.get_width()
-screen.blit(bg, (0,0))
 
-tiles = math.ceil(SCREEN_WIDTH / bg_width) + 1
-
-
+tiles = math.ceil(WIDTH/ bg_width) + 1
 
 run = True
 while run:
-    #timer.tick(fps)
+
     for event in pygame.event.get():
         if event.type ==pygame.QUIT:
-            run = False
+            pygame.display.flip()
+            pygame.quit()
+
+    
+
+    #Bevegelse
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        square_x -= speed
+    if keys[pygame.K_RIGHT]:
+        square_x += speed
+
+    screen.blit(bg, (0,0))
+
+    square_x = max(0, min(WIDTH - square_size, square_x))
+    square_y = max(0, min(HEIGHT - square_size, square_y))
+
+    firkant = pygame.draw.rect(
+        screen,
+        (50, 10, 102),
+        (square_x, square_y, square_size, square_size)
+    )
+
+    sprite = pygame.image.load('Idle.png').convert_alpha()
+    screen.blit(sprite, firkant)
+    
     pygame.display.flip()
-pygame.quit()
-
-
-
-
+    timer.tick
 
